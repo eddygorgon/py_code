5d094a5b7ad93bc1aee4eb6c1dfce18dfaaf5f28
Giorgio Vassallo <giorgio.vassallo@online.de>
2021-08-06 09:44:55 +0200

    Changed comment in ex2

--- ex2_fizz_buzz.py
+++ ex2_fizz_buzz.py
@@ -1,8 +1,10 @@
-# Write a function called fizz_buzz that takes a number.
-# If the number is divisible by 3, it should return “Fizz”.
-# If it is divisible by 5, it should return “Buzz”.
-# If it is divisible by both 3 and 5, it should return “FizzBuzz”.
-# Otherwise, it should return the same number.
+"""
+Write a function called fizz_buzz that takes a number.
+If the number is divisible by 3, it should return “Fizz”.
+If it is divisible by 5, it should return “Buzz”.
+If it is divisible by both 3 and 5, it should return “FizzBuzz”.
+Otherwise, it should return the same number.
+"""
 
 def fizz_buzz(number_in):
     if (number_in%3)==0 and (number_in%5)!=0:
